03b3ddb84bc55ec65d5d88191577f44b08da7ba3
Mir Hussain <mir.hm110@gmail.com>
2021-02-24 18:56:59 +0600

    Added the second section

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
                     <Applicants name={user.name} key={user.id} email={user.email} phone={user.phone}></Applicants>
                 ))}
             </div>
+            <Hudday></Hudday>
         </div>
     );
 }
@@ -26,6 +27,23 @@ function Applicants(props) {
             <h3>Name: {props.name}</h3>
             <p>Email: {props.email}</p>
             <p>Phone: {props.phone}</p>
+        </div>
+    );
+}
+
+function Hudday() {
+    const [count, setCount] = useState(0);
+    return (
+        <div className="hudday">
+            <h1>This section is useless and totally not related to the section above.</h1>
+            <p>I don't wanted to make another react app (Thank you bro for understanding)</p>
+            <button onClick={() => setCount(count + 1)}>Click me for no reason</button>
+            <button onClick={() => setCount(count - 1)}>Fuck, decrease it.</button>
+            <button onClick={() => setCount(0)}>Oh shit, Clear that usless number</button>
+            <h2>useless number: {count}</h2>
+            {count < 0 && 
+                <h2>Nigga, you are in negative numbers. We aren't counting your girlfriends.</h2>
+            }
         </div>
     );
 }
