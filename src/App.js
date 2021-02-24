94cb04fc5a75335a6df4573d62b84d44a7828035
Mir Hussain <mir.hm110@gmail.com>
2021-02-24 14:35:15 +0600

    Added the applicants card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }, []);
+    return (
+        <div>
+            <h1 className="header"> Job Applicants </h1>
+            <div className="card-container">
+                {users.map((user) => (
+                    <Applicants name={user.name} key={user.id} email={user.email} phone={user.phone}></Applicants>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function Applicants(props) {
+    return (
+        <div className="card">
+            <h3>Name: {props.name}</h3>
+            <p>Email: {props.email}</p>
+            <p>Phone: {props.phone}</p>
+        </div>
+    );
 }
 
 export default App;
